2bfe242f6aa4cae5222cfc6918684e9f813305aa
Jaikiran Pai <jaikiran@apache.org>
2022-01-15 10:18:47 +0530

    fix env setting

--- src/script/ant.bat
+++ src/script/ant.bat
@@ -125,10 +125,10 @@ if "%_JAVACMD%" == "" set _JAVACMD=java.exe
 :setSecurityManagerOpt
 echo "Checking Java version"
 "%_JAVACMD%" -XshowSettings:properties 2>&1 | find "java.specification.version = 18"
-set "err_level=%ErrorLevel%"
+set err_level="%ErrorLevel%"
 if %err_level% == 0 (
     rem This is Java 18, so set -Djava.security.manager=allow
-    set "ANT_OPTS=%ANT_OPTS% -Djava.security.manager=allow"
+    set ANT_OPTS="%ANT_OPTS% -Djava.security.manager=allow"
     echo "Set ant opts to %ANT_OPTS%"
 ) else (
     echo "Error level was %err_level%"
