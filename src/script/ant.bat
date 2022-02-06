b3b3c1bf6c0a47341a30c536ba98fa18b1f57ab3
Jaikiran Pai <jaikiran@apache.org>
2022-02-06 08:55:57 +0530

    ! instead of %

--- src/script/ant.bat
+++ src/script/ant.bat
@@ -125,18 +125,18 @@ if "%_JAVACMD%" == "" set _JAVACMD=java.exe
 :setSecurityManagerOpt
 "%_JAVACMD%" -XshowSettings:properties 2>&1
 SETLOCAL EnableDelayedExpansion
-"%_JAVACMD%" -XshowSettings:properties 2>&1 | find "java.specification.version = 18"
+"!_JAVACMD!" -XshowSettings:properties 2>&1 | find "java.specification.version = 18"
 if !errorlevel! EQU 0 (
     echo "set java 18"
     rem This is Java 18, so set -Djava.security.manager=allow
-    set ANT_OPTS=%ANT_OPTS% -Djava.security.manager=allow
+    set ANT_OPTS=!ANT_OPTS! -Djava.security.manager=allow
 ) else (
     echo "checking java 19"
-    "%_JAVACMD%" -XshowSettings:properties 2>&1 | find "java.specification.version = 19"
+    "!_JAVACMD!" -XshowSettings:properties 2>&1 | find "java.specification.version = 19"
     if !errorlevel! EQU 0 (
         echo "set java 19"
         rem This is Java 19, so set -Djava.security.manager=allow
-        set ANT_OPTS=%ANT_OPTS% -Djava.security.manager=allow
+        set ANT_OPTS=!ANT_OPTS! -Djava.security.manager=allow
     )
 )
 endlocal
